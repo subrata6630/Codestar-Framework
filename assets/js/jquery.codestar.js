ae7d60b2b909b8c0efcdd260681f8ba153beed4e
Codestar <info@codestarlive.com>
2015-01-18 15:29:56 +0200

    update

--- assets/js/jquery.codestar.js
+++ assets/js/jquery.codestar.js
@@ -5,6 +5,13 @@
 ;(function ( $, window, document, undefined ) {
   'use strict';
 
+  function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+
+  var blockflood = true;
+
   $(document).ready( function(){
 
     $('#fancybox-gallery a')
@@ -44,6 +51,37 @@
           }
         }
       });
+
+    $('#cs-newsletter').on('click', function( e ) {
+
+      e.preventDefault();
+
+      var $this  = $(this),
+          $email = $('#cs-email');
+
+      if( blockflood && validateEmail( $email.val() ) ) {
+
+        blockflood = false;
+
+        $.ajax({
+          type: 'POST',
+          url: 'https://docs.google.com/forms/d/1-D2aexSmgc3fvPUSZjiwPeaLw9x29j9aMzEzCGcfWmc/formResponse',
+          data: {
+            'entry.1752039613': $email.val(),
+          },
+          success: function( html ) {
+            $email.val('');
+            $this.text('Success!');
+            console.log( html );
+            blockflood = true;
+          },
+        });
+
+      } else {
+        alert( 'Please check your email address!' );
+      }
+
+    });
 
   });
 
