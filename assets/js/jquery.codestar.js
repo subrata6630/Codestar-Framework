3d93a0332bb4faa5493a94ce536eabedbe520085
Codestar <info@codestarlive.com>
2015-01-18 15:46:33 +0200

    update

--- assets/js/jquery.codestar.js
+++ assets/js/jquery.codestar.js
@@ -5,12 +5,10 @@
 ;(function ( $, window, document, undefined ) {
   'use strict';
 
-  function validateEmail(email) {
+  function validateEmail( email ) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
   }
-
-  var blockflood = true;
 
   $(document).ready( function(){
 
@@ -54,32 +52,8 @@
 
     $('#cs-newsletter').on('click', function( e ) {
 
-      e.preventDefault();
-
-      var $this  = $(this),
-          $email = $('#cs-email');
-
-      if( blockflood && validateEmail( $email.val() ) ) {
-
-        blockflood = false;
-
-        $.ajax({
-          type: 'POST',
-          url: 'https://docs.google.com/forms/d/1-D2aexSmgc3fvPUSZjiwPeaLw9x29j9aMzEzCGcfWmc/formResponse',
-          data: {
-            'entry.1752039613': $email.val(),
-          },
-          success: function( html ) {
-            $email.val('');
-            $this.text('Success!');
-            console.log( html );
-            blockflood = true;
-          },
-          dataType: "xml",
-          crossDomain: true,
-        });
-
-      } else {
+      if( !validateEmail( $('#cs-email').val() ) ) {
+        e.preventDefault();
         alert( 'Please check your email address!' );
       }
 
