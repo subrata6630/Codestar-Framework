c04df0303ffe571c9889cf65088255e4fd40da8b
Codestar <info@codestarlive.com>
2015-05-16 13:14:29 +0300

    update

--- cs-framework-path.php
+++ cs-framework-path.php
@@ -38,9 +38,14 @@ if( ! function_exists( 'cs_get_path_locate' ) ) {
     }
 
     if ( ! isset( $basename ) ) {
-      $basename  = str_replace( wp_normalize_path( get_template_directory() ), '', wp_normalize_path( dirname( __FILE__ ) ) );
-      $dir       = get_template_directory() . $basename;
-      $uri       = get_template_directory_uri() . $basename;
+      $dirname        = wp_normalize_path( dirname( __FILE__ ) );
+      $plugin_dir     = wp_normalize_path( WP_PLUGIN_DIR );
+      $located_plugin = ( preg_match( '#'. $plugin_dir .'#', $dirname ) ) ? true : false;
+      $directory      = ( $located_plugin ) ? $plugin_dir : get_template_directory();
+      $directory_uri  = ( $located_plugin ) ? WP_PLUGIN_URL : get_template_directory_uri();
+      $basename       = str_replace( wp_normalize_path( $directory ), '', $dirname );
+      $dir            = $directory . $basename;
+      $uri            = $directory_uri . $basename;
     }
 
     return apply_filters( 'cs_get_path_locate', array(
