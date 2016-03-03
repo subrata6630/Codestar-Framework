f0be427d81f81baa338813d0a16c40d1217a3337
Nguyen Van Anh <anhskohbo@gmail.com>
2016-03-03 11:47:24 +0700

    Missing open bracket placeholder

--- functions/actions.php
+++ functions/actions.php
@@ -56,7 +56,7 @@ if( ! function_exists( 'cs_set_icons' ) ) {
   function cs_set_icons() {
 
     echo '<div id="cs-icon-dialog" class="cs-dialog" title="'. __( 'Add Icon', 'cs-framework' ) .'">';
-    echo '<div class="cs-dialog-header cs-text-center"><input type="text" placeholder='. __( 'Search a Icon...', 'cs-framework' ) .'" class="cs-icon-search" /></div>';
+    echo '<div class="cs-dialog-header cs-text-center"><input type="text" placeholder="'. __( 'Search a Icon...', 'cs-framework' ) .'" class="cs-icon-search" /></div>';
     echo '<div class="cs-dialog-load"><div class="cs-icon-loading">'. __( 'Loading...', 'cs-framework' ) .'</div></div>';
     echo '</div>';
 
