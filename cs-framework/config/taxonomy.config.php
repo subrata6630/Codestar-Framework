8a2d58f2017a0de19295fb5a3fba57530e4ca22d
Codestar <codestarlive@gmail.com>
2016-04-02 20:07:16 +0300

    added taxonomy framework feature

---
+++ cs-framework/config/taxonomy.config.php
@@ -0,0 +1,52 @@
+<?php if ( ! defined( 'ABSPATH' ) ) { die; } // Cannot access pages directly.
+// ===============================================================================================
+// -----------------------------------------------------------------------------------------------
+// TAXONOMY OPTIONS
+// -----------------------------------------------------------------------------------------------
+// ===============================================================================================
+$options     = array();
+
+// -----------------------------------------
+// Taxonomy Options                        -
+// -----------------------------------------
+$options[]   = array(
+  'id'       => '_custom_category_options',
+  'title'    => 'Custom Category Options',
+  'taxonomy' => 'category',
+
+  // begin: fields
+  'fields'   => array(
+
+    // begin: a field
+    array(
+      'id'    => 'section_1_text',
+      'type'  => 'text',
+      'title' => 'Text Field',
+    ),
+    // end: a field
+
+    array(
+      'id'    => 'section_1_textarea',
+      'type'  => 'textarea',
+      'title' => 'Textarea Field',
+    ),
+
+  ), // end: fields
+);
+
+$options[]   = array(
+  'id'       => '_custom_tag_options',
+  'title'    => 'Custom Tags Options',
+  'taxonomy' => 'post_tag',
+  'fields'   => array(
+
+    array(
+      'id'    => 'section_1_text',
+      'type'  => 'text',
+      'title' => 'Text Field',
+    ),
+
+  ),
+);
+
+CSFramework_Taxonomy::instance( $options );
--- cs-framework/cs-framework.php
+++ cs-framework/cs-framework.php
@@ -48,6 +48,7 @@ if( ! function_exists( 'cs_framework_init' ) && ! class_exists( 'CSFramework' ) 
     // active modules
     defined( 'CS_ACTIVE_FRAMEWORK' )  or  define( 'CS_ACTIVE_FRAMEWORK',  true );
     defined( 'CS_ACTIVE_METABOX'   )  or  define( 'CS_ACTIVE_METABOX',    true );
+    defined( 'CS_ACTIVE_TAXONOMY'   ) or  define( 'CS_ACTIVE_TAXONOMY',   true );
     defined( 'CS_ACTIVE_SHORTCODE' )  or  define( 'CS_ACTIVE_SHORTCODE',  true );
     defined( 'CS_ACTIVE_CUSTOMIZE' )  or  define( 'CS_ACTIVE_CUSTOMIZE',  true );
 
@@ -64,12 +65,14 @@ if( ! function_exists( 'cs_framework_init' ) && ! class_exists( 'CSFramework' ) 
     cs_locate_template( 'classes/options.class.php'    );
     cs_locate_template( 'classes/framework.class.php'  );
     cs_locate_template( 'classes/metabox.class.php'    );
+    cs_locate_template( 'classes/taxonomy.class.php'   );
     cs_locate_template( 'classes/shortcode.class.php'  );
     cs_locate_template( 'classes/customize.class.php'  );
 
     // configs
     cs_locate_template( 'config/framework.config.php'  );
     cs_locate_template( 'config/metabox.config.php'    );
+    cs_locate_template( 'config/taxonomy.config.php'   );
     cs_locate_template( 'config/shortcode.config.php'  );
     cs_locate_template( 'config/customize.config.php'  );
 
