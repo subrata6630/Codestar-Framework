2b27a177aeace506a8e863528ccf0cdaa85c4ba8
Codestar <codestarlive@gmail.com>
2016-04-05 14:04:02 +0300

    added framework title and settings improve

--- config/framework.config.php
+++ config/framework.config.php
@@ -4,11 +4,13 @@
 // FRAMEWORK SETTINGS
 // -----------------------------------------------------------------------------------------------
 // ===============================================================================================
-$settings      = array(
-  'menu_title' => 'Framework',
-  'menu_type'  => 'add_menu_page',
-  'menu_slug'  => 'cs-framework',
-  'ajax_save'  => false,
+$settings           = array(
+  'menu_title'      => 'Framework',
+  'menu_type'       => 'menu', // menu, submenu, options, theme, etc.
+  'menu_slug'       => 'cs-framework',
+  'ajax_save'       => false,
+  'show_reset_all'  => false,
+  'framework_title' => 'Codestar Framework <small>by Codestar</small>',
 );
 
 // ===============================================================================================
