0cca22b67a6194824d058bee73594746a048e19a
Codestar <codestarlive@gmail.com>
2016-04-13 15:27:50 +0300

    updated plugin version

--- cs-framework/cs-framework.php
+++ cs-framework/cs-framework.php
@@ -10,7 +10,7 @@
  * Plugin URI: http://codestarframework.com/
  * Author: Codestar
  * Author URI: http://codestarlive.com/
- * Version: 1.0.0
+ * Version: 1.0.1
  * Description: A Lightweight and easy-to-use WordPress Options Framework
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
